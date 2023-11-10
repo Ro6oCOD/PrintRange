b870cedbbd37d2ce53ed356d2d448c3c6d5e4405
Ro6oCOD <145925739+Ro6oCOD@users.noreply.github.com>
2023-11-10 20:07:28 +0700

    Update PrintRange.cpp

--- PrintRange.cpp
+++ PrintRange.cpp
@@ -6,20 +6,26 @@
 
 using namespace std;
 
-template <typename It>
-void PrintRange(It range_begin, It range_end) {
-    for (auto it = range_begin; it != range_end; it++) {
+template<typename Iterator>
+void PrintRange(Iterator container_begin, Iterator container_end) {
+    for (auto it = container_begin; it != container_end; ++it) {
         cout << *it << " "s;
     }
     cout << endl;
 }
 
+template <typename Container, typename Element>
+void FindAndPrint(Container container, Element element) {
+    auto pos = find(container.begin(), container.end(), element);
+    PrintRange(container.begin(), pos);
+    PrintRange(pos, container.end());
+}
+
 int main() {
+    set<int> test = {1, 1, 1, 2, 3, 4, 5, 5};
     cout << "Test1"s << endl;
-    set<int> test1 = {1, 1, 1, 2, 3, 4, 5, 5};
-    PrintRange(test1.begin(), test1.end());
+    FindAndPrint(test, 3);
     cout << "Test2"s << endl;
-    vector<int> test2 = {}; // пустой контейнер
-    PrintRange(test2.begin(), test2.end());
+    FindAndPrint(test, 0); // элемента 0 нет в контейнере
     cout << "End of tests"s << endl;
 }
